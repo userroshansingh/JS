da509e140956d907ba2c6f78aabf2f683cdb8439
Roshan Singh <userroshansingh@gmail.com>
2025-01-10 22:44:29 +0530

    modified

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,3 +13,6 @@ accountCity = "Gurgaon"
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// prefer not to use var 
+// because of issue in block scope and functional scope

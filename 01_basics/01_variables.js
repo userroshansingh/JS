72eb6fdb7fec8c9795f31099c7817b658b74c1fd
Roshan Singh <userroshansingh@gmail.com>
2025-01-10 22:12:40 +0530

    added 01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 250307
+let accountEmail = "roshan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2 not allowed because declared const previously
+
+console.log(accountId);
+
+accountEmail = "rds@gmail.com"
+accountPassword = "98765"
+accountCity = "Gurgaon"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
